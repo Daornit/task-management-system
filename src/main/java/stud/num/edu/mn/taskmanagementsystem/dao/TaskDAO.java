9c64cee143646fb0ee92f97e7bda7a4215c861d7
Barkhasbold <43365467+Barkhasbold@users.noreply.github.com>
2019-11-27 18:14:12 +0800

    Update TaskDAO.java

--- src/main/java/stud/num/edu/mn/taskmanagementsystem/dao/TaskDAO.java
+++ src/main/java/stud/num/edu/mn/taskmanagementsystem/dao/TaskDAO.java
@@ -5,7 +5,7 @@ import stud.num.edu.mn.taskmanagementsystem.entity.ImsUser;
 import stud.num.edu.mn.taskmanagementsystem.entity.work.Task;
 
 import java.util.List;
-
+//Өгөгдлийн сантай холболт үүсгэн query хийх боломж олгоно. 
 public interface TaskDAO extends PagingAndSortingRepository<Task, Long> {
     Task findByCode(String code);
     List<Task> findAllByOwnerAndIsDeleted(ImsUser user, Boolean deleted);
